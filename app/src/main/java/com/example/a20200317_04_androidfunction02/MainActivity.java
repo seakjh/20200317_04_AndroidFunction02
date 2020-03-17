d8790491906260707b639e7c11572b0f3168e636
seakjh <skykjh0513@gmail.com>
2020-03-17 17:16:23 +0900

    전화 걸기 직전 화면 추가

--- app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
+++ app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.a20200317_04_androidfunction02;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.a20200317_04_androidfunction02.databinding.ActivityMainBinding;
 
@@ -15,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        
+
+        binding.dialbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                다른화면으로 이동 intnent
+//                => 우리가 만든게 아니라, 안드로이드가 제공하는 화면으로 이동.
+//                전화걸기 화면
+
+//
+                Uri uri = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }

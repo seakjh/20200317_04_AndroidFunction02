632a17974363af8350695a0fb606846a0af49bcc
seakjh <skykjh0513@gmail.com>
2020-03-17 17:45:08 +0900

    전화 직접 거는 코드 권한문제로 지금은 실행 불가

--- app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
+++ app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.a20200317_04_androidfunction02.databinding.ActivityMainBinding;
@@ -26,9 +27,29 @@ public class MainActivity extends AppCompatActivity {
 //                => 우리가 만든게 아니라, 안드로이드가 제공하는 화면으로 이동.
 //                전화걸기 화면
 
-//
-                Uri uri = Uri.parse("tel:01012345678");
+//              전화번호가 뭐라고 입력되어있는지 받아오자
+//                받아온 전화번호를 호출
+
+                String inputNum = binding.inputEdt.getText().toString();
+                Log.d("입력한번호", inputNum);
+
+//                문자를 숫자로
+                String phoneUri = String.format("tel:%s",inputNum);
+
+                Uri uri = Uri.parse(phoneUri);
                 Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        binding.callbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                현재까지 배운것만으론 실행 불가(권한 획득을 못함)
+
+                Uri uri = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
                 startActivity(intent);
             }
         });
